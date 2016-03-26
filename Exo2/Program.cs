ac6c7abd29efb2a6047dc10394b1b93037644192
unknown <maxime.neuder@gmail.com>
2016-03-26 10:45:47 +0100

    chargement

---
+++ Exo2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nombres = {5,4,1,3,9,8,6,7,2,0};
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+         /*   int nombresPaires = nombres.Count(n => n % 2 ==0);
+
+            Console.WriteLine(nombresPaires);
+
+            Console.ReadLine(); */
+
+           /* IEnumerable nPremier = nombres.Where(n => n % 2 != 0 && n > 6);
+
+            foreach (var v in nPremier)
+                Console.WriteLine(v);*/
+
+           /* var lesNombres = nombres.TakeWhile((n, index) => n >= index);
+
+            foreach (var l in lesNombres)
+                Console.WriteLine(l);*/
+
+           /* int ScoreCount = scores.Where(n => n > 80).Count();
+
+            Console.WriteLine(ScoreCount);*/
+
+            var lesScoreCount = scores.Where(n => n > 80);
+
+            foreach (var v in lesScoreCount)
+                Console.WriteLine(v);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TpLambda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLambda
+{
+    class Program
+    {
+        delegate int monDelegate(int i);
+        delegate bool monDelegate2(int i, int j);
+        delegate bool monDelegate3(int i, string c);
+        delegate string monDelegate4(string s);
+        delegate bool monDelegate5(int i);
+        delegate string monDelegate6(string c);
+        delegate int monDelegate7();
+        delegate int monDelegate8(int i, int m, int n);
+        delegate List<char> monDelegate9(string c);
+        static void Main(string[] args)
+        {
+            monDelegate9 del = (s) => { Console.WriteLine(s); foreach (char c in s) { } };
+
+            Func<string, List<char>> f = (s) => { Console.WriteLine(s); foreach (char c in s) { Console.WriteLine(c); } };
+
+            List<char> result = f("hello");
+
+            Console.WriteLine(result);
+           
+            Console.ReadLine();
+        }
+    }
+}
